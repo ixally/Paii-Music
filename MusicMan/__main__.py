1c8a8e2d1da382960620a0580989c8b84296fd4a
ixally <xallmean@gmail.com>
2025-11-01 06:19:50 +0700

    Update __main__.py

--- MusicMan/__main__.py
+++ MusicMan/__main__.py
@@ -3,8 +3,7 @@ import requests
 from pyrogram import Client as Bot
 
 from MusicMan.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from MusicMan.services.callsmusic import run
-
+from MusicMan.services.callsmusic.callsmusic import pytgcalls
 # Download background image
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
