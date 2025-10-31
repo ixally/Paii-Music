c9d38f97922bfd65e1b573d5c202da8c5782c8c6
ixally <xallmean@gmail.com>
2025-10-31 15:52:41 +0700

    Update callsmusic.py

--- MusicMan/services/callsmusic/callsmusic.py
+++ MusicMan/services/callsmusic/callsmusic.py
@@ -37,7 +37,7 @@ async def on_stream_end(call, is_connected):
 # --- Runner ---
 async def run():
     await client.start()
-    await pytgcalls.start()
+    await pytgcalls.start(-1003144399235)
     print("✅ PyTgCalls connected successfully!")
     await idle()
 
